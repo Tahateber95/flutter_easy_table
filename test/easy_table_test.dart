271787f3268d7602cc12b85554993ebcc1008821
Tahateber95 <mr.layto@gmail.com>
2022-08-02 22:27:17 +0000

    removed import

--- test/easy_table_test.dart
+++ test/easy_table_test.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:easy_table/easy_table.dart';
-
 void main() {
   test('adds one to input values', () {
     // final calculator = Calculator();
